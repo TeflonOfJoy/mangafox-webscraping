b06d6973525d36db24afd71e5ced973f784a6757
Tef <39195498+TeflonOfJoy@users.noreply.github.com>
2020-08-22 16:54:44 +0200

    Added image scraping

--- src/WebScrape.java
+++ src/WebScrape.java
@@ -3,10 +3,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.*;
+import java.net.URL;
 
 public class WebScrape {
     public static void main(String[] args) {
-        try {
+        /*try {
             File manga = new File("scrape.txt");
             if (manga.createNewFile()) {
                 System.out.println("File created");
@@ -19,22 +20,45 @@ public class WebScrape {
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
-        }
+        }*/
         int i = 1;
         String url;
-        while ( i < 394){
-            url = "http://fanfox.net/directory/" + String.valueOf(i) + ".html";
+        final String folderPath = "C:\\Users\\Fernand\\Desktop\\mangafox-webscraping-master\\img\\";
+        while ( i < 396){
+            url = "http://fanfox.net/directory/" + i + ".html";
             try {
                 final Document document = Jsoup.connect(url).get();
-                BufferedWriter writer = new BufferedWriter(new FileWriter("scrape.txt", true));
                 Elements a = document.select("body.bg-gary div.container div.line-list div.manga-list-1 ul.manga-list-1-list.line li p.manga-list-1-item-title a");
+                Elements img = document.select("body.bg-gary div.container div.line-list div.manga-list-1 ul.manga-list-1-list.line li a img.manga-list-1-cover");
+                /*BufferedWriter writer = new BufferedWriter(new FileWriter("scrape.txt", true));
                 for (Element element : a){
                     writer.append(element.attr("title"));
                     writer.append(" ");
                     writer.append("http://fanfox.net").append(element.attr("href"));
                     writer.newLine();
                 }
-                writer.close();
+                writer.close();*/
+                for (Element element : img){
+                    URL urli = new URL(element.attr("src"));
+                    String name = element.attr("alt");
+                    if (name.contains("?") || name.contains("!") || name.contains("<") || name.contains(">") || name.contains("*") || name.contains("\\") || name.contains(":") || name.contains("/")){
+                        name = name.replace("?", "");
+                        name = name.replace("!", "");
+                        name = name.replace("<", "");
+                        name = name.replace(">", "");
+                        name = name.replace("*", "");
+                        name = name.replace("\\", "");
+                        name = name.replace(":", "");
+                        name = name.replace("/", "");
+                    }
+                    InputStream in = urli.openStream();
+                    OutputStream out = new BufferedOutputStream(new FileOutputStream( folderPath + name + ".png"));
+                    for (int b; (b = in.read()) != -1;) {
+                        out.write(b);
+                    }
+                    out.close();
+                    in.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
